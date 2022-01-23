040370a31604168f39c3ec64c037804d8fd94c31
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-23 17:03:30 -0600

    Fix component name and fill required style files

---
+++ src/components/ShoppingCartItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShoppingCartItem = () => {
+  return(
+    <h1>This is the ShoppingCartItem component.</h1>
+  );
+}
+
+export default ShoppingCartItem;
--- src/components/ShoppingCartitem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ShoppingCartitem = () => {
-  return(
-    <h1>This is the ShoppingCartitem component.</h1>
-  );
-}
-
-export default ShoppingCartitem;
