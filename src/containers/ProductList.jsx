6e89e9001cbe938cd2c848f86a055ad93c8b35dd
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-24 18:21:02 -0600

    Add axios to fetch data from API with useEffect

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(async() => {
+    const response = await axios(API);
+    setProducts(response.data);
+  }, [])
+
   return(
     <section className="main-container">
       <div className="ProductList">
-        <ProductItem />
+        {products.map(product => (
+          <ProductItem />
+        ))}
       </div>
     </section>
   );
