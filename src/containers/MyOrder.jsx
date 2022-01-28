ed277cbec74d1fd689d7bb816d802b705c087e84
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-28 16:06:54 -0600

    Fill cart modal with API data and total sum

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -7,6 +7,12 @@ import flechita from '@icons/flechita.svg';
 const MyOrder = () => {
   const { state } = useContext(AppContext);
 
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return(
     <aside className="MyOrder">
       <div className="title-container">
@@ -21,7 +27,7 @@ const MyOrder = () => {
 	  <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>{sumTotal()}</p>
         </div>
         <button className="primary-button">
           Checkout
