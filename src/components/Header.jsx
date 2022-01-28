4a2d7f1dc02c4ac9df2652df22701667d9ffe003
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-27 18:36:55 -0600

    Implement useContext on Header to catch number of cart items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Menu from '@components/Menu';
+import AppContext from '@context/AppContext';
 import '@styles/Header.scss';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 import menu from '@icons/icon_menu.svg'; 
@@ -7,6 +8,7 @@ import logo from '@logos/logo_yard_sale.svg';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const { state } = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -45,7 +47,7 @@ const Header = () => {
         </li>
         <li className="navbar-shopping-cart">
           <img src={shoppingCart} alt="shopping cart" />
-          <div>2</div>
+          {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
         </li>
       </ul>
     </div>
